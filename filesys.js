3a13a7270c4a3cccfe9201000436fa205fdd0f1e
skbaghla <er.skbaghla@gmail.com>
2024-05-21 10:40:14 -0400

    File System

--- filesys.js
+++ filesys.js
@@ -1,15 +1,32 @@
 const fs = require('fs');
 
-const input = process.argv;
+const path = require('path');
 
-if (input[2] == 'add') {
-    fs.writeFileSync(input[3], input[4]);
-}
-else if (input[2] == 'remove') {
-    fs.unlinkSync(input[3]);
-}
-else {
-    console.log("Invalid Output");
-}
-//to add file :: node .\filesys.js add output.txt 'this is a data file'
-//to remove : node .\filesys.js remove output.txt
+const dirPath = path.join(__dirname, 'files');
+
+
+// for (i = 0; i < 5; i++) {
+// // fs.writeFileSync(`Hello ${i}.txt`, "A Simple Text File.");
+// fs.writeFileSync(dirPath+"/hello "+i+".txt", "A Simple Text File.");
+// }
+// console.warn(dirPath);
+
+fs.readdir(dirPath,(error,files)=>{
+    files.forEach((items)=>{
+        console.log(`file name is ${items}`);
+    })
+});
+
+// const input = process.argv;
+
+// if (input[2] == 'add') {
+//     fs.writeFileSync(input[3], input[4]);
+// }
+// else if (input[2] == 'remove') {
+//     fs.unlinkSync(input[3]);
+// }
+// else {
+//     console.log("Invalid Output");
+// }
+// //to add file :: node .\filesys.js add output.txt 'this is a data file'
+// //to remove : node .\filesys.js remove output.txt
