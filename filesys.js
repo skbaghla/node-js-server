f6583ccdf70e0bce9166e720096c626a3b52447b
skbaghla <er.skbaghla@gmail.com>
2024-05-21 15:19:33 -0400

    Creating, Updating, renaming and deleting in File system.

--- filesys.js
+++ filesys.js
@@ -1,32 +1,54 @@
 const fs = require('fs');
 
 const path = require('path');
+const dirPath = path.join(__dirname, 'files/crud');
+const filePath = `${dirPath}/apple.txt`;
 
-const dirPath = path.join(__dirname, 'files');
 
+// fs.writeFileSync(filePath,"This is a simple text file.")
 
-// for (i = 0; i < 5; i++) {
-// // fs.writeFileSync(`Hello ${i}.txt`, "A Simple Text File.");
-// fs.writeFileSync(dirPath+"/hello "+i+".txt", "A Simple Text File.");
-// }
-// console.warn(dirPath);
+// //reading file:
+// fs.readFile(filePath,'utf8',(err,item)=>{
+//     console.log(item);
+// })
 
-fs.readdir(dirPath,(error,files)=>{
-    files.forEach((items)=>{
-        console.log(`file name is ${items}`);
-    })
-});
+// //appending an File:
+// fs.appendFile(filePath,' and file name is apple.txt ',(err)=>{
+//     if(!err) console.log("file is updated.")
+// });
 
-// const input = process.argv;
+//Rename file name
 
-// if (input[2] == 'add') {
-//     fs.writeFileSync(input[3], input[4]);
-// }
-// else if (input[2] == 'remove') {
-//     fs.unlinkSync(input[3]);
-// }
-// else {
-//     console.log("Invalid Output");
-// }
-// //to add file :: node .\filesys.js add output.txt 'this is a data file'
-// //to remove : node .\filesys.js remove output.txt
+// fs.rename(filePath,`${dirPath}/fruit.txt`,(err)=>{
+//     console.log("File renamed successfully.")
+// });
+
+//-----deleting the record:
+
+fs.unlinkSync(`${dirPath}/fruit.txt`);
+
+// // for (i = 0; i < 5; i++) {
+// // // fs.writeFileSync(`Hello ${i}.txt`, "A Simple Text File.");
+// // fs.writeFileSync(dirPath+"/hello "+i+".txt", "A Simple Text File.");
+// // }
+// // console.warn(dirPath);
+
+// fs.readdir(dirPath,(error,files)=>{
+//     files.forEach((items)=>{
+//         console.log(`file name is ${items}`);
+//     })
+// });
+
+// // const input = process.argv;
+
+// // if (input[2] == 'add') {
+// //     fs.writeFileSync(input[3], input[4]);
+// // }
+// // else if (input[2] == 'remove') {
+// //     fs.unlinkSync(input[3]);
+// // }
+// // else {
+// //     console.log("Invalid Output");
+// // }
+// // //to add file :: node .\filesys.js add output.txt 'this is a data file'
+// // //to remove : node .\filesys.js remove output.txt
